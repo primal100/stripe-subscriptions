758fe20e92ea879fed671c036ebeeb66779f36f9
Paul Martin <greatestloginnameever@gmail.com>
2021-04-27 05:37:36 +0100

    Fix setup.py versioning

--- setup.py
+++ setup.py
@@ -1,13 +1,17 @@
 from setuptools import setup
-from subscriptions import version
 
 
 readme = open('README.md', 'r').read()
 
 
+# https://stackoverflow.com/questions/2058802/how-can-i-get-the-version-defined-in-setup-py-setuptools-in-my-package
+main_ns = {}
+with open('subscriptions/__version__.py') as f:
+    exec(f.read(), main_ns)
+
 setup(
     name='stripe-subscriptions',
-    version=version,
+    version=main_ns['__version__'],
     packages=['subscriptions'],
     url='https://github.com/primal100/stripe-subscriptions',
     license="MIT License",
