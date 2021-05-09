c1836f115cefc057fece4685652fbe8d3584e876
Paul Martin <greatestloginnameever@gmail.com>
2021-05-09 08:32:33 +0100

    Add setup checkouts

--- tests/test_subscriptions.py
+++ tests/test_subscriptions.py
@@ -8,13 +8,14 @@ def test_create_customer_user(user):
     assert user.stripe_customer_id
 
 
-def test_create_checkout_session(user_with_customer_id, stripe_price_id, checkout_success_url,
-                                 checkout_cancel_url, payment_method_types):
+def test_create_subscription_checkout_session(user_with_customer_id, stripe_price_id, checkout_success_url,
+                                              checkout_cancel_url, payment_method_types):
     checkout = subscriptions.create_subscription_checkout(user_with_customer_id, stripe_price_id,
                                                           success_url=checkout_success_url,
                                                           cancel_url=checkout_cancel_url,
                                                           payment_method_types=payment_method_types)
     assert checkout['id'] is not None
+    assert checkout['setup_intent'] is None
     assert checkout['success_url'] == checkout_success_url
     assert checkout['cancel_url'] == checkout_cancel_url
 
@@ -22,6 +23,18 @@ def test_create_checkout_session(user_with_customer_id, stripe_price_id, checkou
 def test_create_checkout_session_no_customer_id_fails(user, stripe_price_id):
     with pytest.raises(subscriptions.StripeCustomerIdRequired):
         subscriptions.create_subscription_checkout(user, stripe_price_id)
+
+
+def test_create_setup_checkout_session(user_with_customer_id, checkout_success_url,
+                                       checkout_cancel_url, payment_method_types):
+    checkout = subscriptions.create_setup_checkout(user_with_customer_id,
+                                                   success_url=checkout_success_url,
+                                                   cancel_url=checkout_cancel_url,
+                                                   payment_method_types=payment_method_types)
+    assert checkout['id'] is not None
+    assert checkout['setup_intent'] is not None
+    assert checkout['success_url'] == checkout_success_url
+    assert checkout['cancel_url'] == checkout_cancel_url
 
 
 def test_is_subscribed(subscribed_user, stripe_subscription_product_id):
